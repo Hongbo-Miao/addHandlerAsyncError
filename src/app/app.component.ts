52e51c5dbcdb39d0fcf26b1453c008ed077aaddb
Brandon Koch <t-brkoc@microsoft.com>
2017-06-20 14:35:48 -0700

    Code for simple reproduction of error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,50 @@
 import { Component } from '@angular/core';
+import { ExcelService } from './services/excel.service';
+import { IOfficeResult } from './services/ioffice-result';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: 
+  `<h1>{{name}}</h1>
+  <button 
+  class="ms-Button ms-Button--primary" 
+  type="submit"
+  (click)="onBind()"
+  ><span class="ms-Button-label">Bind to A1</span></button>
+  <button 
+  class="ms-Button ms-Button--primary" 
+  type="submit"
+  (click)="addHandler()"
+  ><span class="ms-Button-label">Add handler to A1</span></button>
+  <p>{{feedback | json}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Demo of addHandlerAsync Error';
+  feedback = '';
+
+  constructor(private excelService: ExcelService) {}
+
+  onBind(){
+    this.excelService
+    .bindToWorkBook()
+    .then((result: IOfficeResult) => {
+        this.feedback = result.success;
+    }, (result: IOfficeResult) => {
+        this.feedback = result.error;
+    });
+  }
+
+  addHandler() {
+    this.excelService.createHandler()
+    .then((result: Office.AsyncResultStatus) => {
+      this.feedback = result.toString();
+      //this.onResult(result);
+    }, (result: IOfficeResult) => {
+      console.log(result);
+                this.feedback = result.toString();
+              });
+  }
+
+
+}
