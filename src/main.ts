67b0537413c612ce7dfa3e214176d5daef65ec91
Brandon Koch <t-brkoc@microsoft.com>
2017-06-20 14:00:42 -0700

    Setup Office.js

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
+
+// For running within Excel
+Office.initialize = function () {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+};
